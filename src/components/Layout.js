b2cf44907edeb944bb22137e2a7a67d896c11011
yuki nomura <yuki.nomura@aijus.com>
2019-09-06 18:32:01 +0900

    add basic styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,9 @@ import { Helmet } from 'react-helmet'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import './all.sass'
+import '../assets/styles/basic/reset.css'
+import '../assets/styles/basic/base.scss'
+import '../assets/styles/basic/typography.scss'
 import useSiteMetadata from './SiteMetadata'
 import { withPrefix } from "gatsby"
 
